b0572cc7380b0433af9caa46dd5fc00f198be410
G.Ambatte <sean@robertson-family.nz>
2022-04-17 23:24:53 +1200

    Initial code pass, placeholder content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import logo from './logo.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return <div className="srd">
+      <div className="srd-header">
+        <div className="srd-title">
+          <img src={logo} className="srd-logo" alt="logo" />
+          <h1>5E SRD GENERATOR</h1>
+        </div>
+      </div>
+      <div className='srd-body'>
+        <h2>LET'S PUT CONTENT HERE</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec sem congue, ornare neque vel, varius metus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut fringilla non nibh quis gravida. Integer nec viverra diam. Vivamus ultricies nisl non metus placerat laoreet. Integer lobortis nisl sollicitudin sollicitudin condimentum. Ut ornare, sapien eu ultricies feugiat, est ante iaculis turpis, non gravida odio elit vel justo. Quisque nec nibh nec sem fermentum malesuada id at nulla.</p>
+        <p>Donec sed lacinia lorem, at mollis purus. Maecenas tellus elit, imperdiet et magna id, ornare dignissim orci. Fusce iaculis fringilla scelerisque. Nunc suscipit accumsan massa, id ultrices mi consequat sed. Praesent porttitor ante id hendrerit aliquet. Donec laoreet felis in tempus molestie. Nullam aliquet bibendum fringilla. Sed imperdiet urna vel ullamcorper commodo. Ut commodo libero ut maximus tempor. Ut sit amet ultricies orci, id suscipit ante. Aenean volutpat, ipsum a ultricies finibus, ex lorem suscipit orci, suscipit bibendum enim augue a odio. Nulla vitae tristique lorem. In nec tellus commodo, faucibus sem ut, pellentesque dolor. Duis hendrerit congue erat, vitae interdum ex tempus at. Donec condimentum nunc vitae libero feugiat, vel venenatis purus faucibus.</p>
+        <p>Integer accumsan sapien sit amet tortor accumsan feugiat ut pretium odio. Cras porttitor ante et aliquam interdum. Nullam efficitur metus sed felis elementum, eu blandit sem varius. Phasellus dapibus, tortor sed laoreet luctus, nisl nisi finibus eros, et egestas elit ex in lacus. Suspendisse ullamcorper tortor libero, eget sodales tellus aliquet eu. Morbi maximus, metus in suscipit semper, velit lectus sagittis tortor, et maximus magna ex in justo. In dapibus molestie risus, eget venenatis sapien fringilla quis. Aenean aliquet enim eu enim auctor, et commodo orci sollicitudin. Donec dictum a elit non consectetur.</p>
+        <p>Donec suscipit tempor dictum. Nam sit amet mauris sem. Nam porta auctor orci et venenatis. Integer nibh elit, sollicitudin nec arcu non, posuere egestas nisi. Nam eleifend magna id porttitor sollicitudin. In hac habitasse platea dictumst. Suspendisse efficitur dapibus lectus eu ornare. Donec accumsan, nibh mattis aliquet sagittis, erat urna consequat magna, et mollis quam ante eu ex. Nam non turpis dignissim, pellentesque eros ut, finibus quam. Pellentesque sed egestas odio. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque mi nunc, condimentum ac erat ut, viverra rutrum leo. Praesent suscipit justo sed lacus consequat, eu laoreet diam faucibus.</p>
+      </div>
     </div>
-  );
-}
+};
 
 export default App;
