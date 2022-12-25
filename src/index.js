899d7b72277ff76fb62f28ff1009d3442298dd76
G.Ambatte <sean@robertson-family.nz>
2022-12-26 11:26:42 +1300

    Change to Hash Routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import './index.css';
 import Paths from './Paths.js';
@@ -9,9 +9,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<HashRouter>
 			<Paths />
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>
 );
 
