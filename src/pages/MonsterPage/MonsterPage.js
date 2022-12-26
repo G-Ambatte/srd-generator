c20813e8b265bff77f801b962324b8ca65f7f3af
G.Ambatte <sean@robertson-family.nz>
2022-12-26 16:44:26 +1300

    Fix typo, move image

--- src/pages/MonsterPage/MonsterPage.js
+++ src/pages/MonsterPage/MonsterPage.js
@@ -19,53 +19,55 @@ const MonsterPage = createReactClass({
 
 		console.log(monster);
 		return <>
-			<img src={monster.img_url} />
-			<h2>{monster.name}</h2>
-			<p><em>{monster.meta}</em></p>
-			<hr />
-			<dl>
-				{['Armor Class', 'Hit Points', 'Speed'].map((item, idx)=>{ return <div key={idx}><dt><strong>{item}</strong></dt><dd>{monster[item]}</dd></div>; })}
-			</dl>
-			<hr />
-			<table>
-				<thead>
-					<tr>
-						{['STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA'].map((item, idx)=>{return <th key={idx} align='center'>{item}</th>;})}
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						{['STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA'].map((item, idx)=>{return <td key={idx} align='center'>{monster[item]} {monster[`${item}_mod`]}</td>;})}
-					</tr>
-				</tbody>
-			</table>
-			<hr />
-			<dl>
-				{['Saving Throws', 'Skills', 'Damage Immunities', 'Condition Immunites', 'Senses', 'Languages', 'Challenge'].map((item, idx)=>{
-					if(!monster[item]) return;
-					return <div key={idx}>
-						<dt key={idx}>
-							<strong>{item}</strong>
-						</dt>
-						<dd>
-							{monster[item]}
-						</dd>
-					</div>;
+			<div className='monster frame'>
+				<img src={monster.img_url} />
+				<h2>{monster.name}</h2>
+				<p><em>{monster.meta}</em></p>
+				<hr />
+				<dl>
+					{['Armor Class', 'Hit Points', 'Speed'].map((item, idx)=>{ return <div key={idx}><dt><strong>{item}</strong></dt><dd>{monster[item]}</dd></div>; })}
+				</dl>
+				<hr />
+				<table>
+					<thead>
+						<tr>
+							{['STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA'].map((item, idx)=>{return <th key={idx} align='center'>{item}</th>;})}
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							{['STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA'].map((item, idx)=>{return <td key={idx} align='center'>{monster[item]} {monster[`${item}_mod`]}</td>;})}
+						</tr>
+					</tbody>
+				</table>
+				<hr />
+				<dl>
+					{['Saving Throws', 'Skills', 'Damage Immunities', 'Condition Immunities', 'Senses', 'Languages', 'Challenge'].map((item, idx)=>{
+						if(!monster[item]) return;
+						return <div key={idx}>
+							<dt key={idx}>
+								<strong>{item}</strong>
+							</dt>
+							<dd>
+								{monster[item]}
+							</dd>
+						</div>;
+					})}
+				</dl>
+				<hr />
+				{monster.Traits.map((trait, idx)=>{
+					return <p key={idx}><em><strong>{trait.title}</strong></em>{trait.content}</p>;
 				})}
-			</dl>
-			<hr />
-			{monster.Traits.map((trait, idx)=>{
-				return <p key={idx}><em><strong>{trait.title}</strong></em>{trait.content}</p>;
-			})}
-			<h3 id='actions'>Actions</h3>
-			{monster.Actions.map((action, idx)=>{
-				return <p key={idx}><em><strong>{action.title}</strong></em>{action.content}</p>;
-			})}
-			<h3 id='actions'>Legendary Actions</h3>
-			{monster['Legendary Actions'].map((legendaryaction, idx)=>{
-				if(!legendaryaction) return;
-				return <p key={idx}><em><strong>{legendaryaction.title}</strong></em>{legendaryaction.content}</p>;
-			})}
+				<h3 id='actions'>Actions</h3>
+				{monster.Actions.map((action, idx)=>{
+					return <p key={idx}><em><strong>{action.title}</strong></em>{action.content}</p>;
+				})}
+				<h3 id='actions'>Legendary Actions</h3>
+				{monster['Legendary Actions'].map((legendaryaction, idx)=>{
+					if(!legendaryaction) return;
+					return <p key={idx}><em><strong>{legendaryaction.title}</strong></em>{legendaryaction.content}</p>;
+				})}
+			</div>
 		</>;
 	},
 
